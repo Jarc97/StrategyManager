15d398942e3a2781ff546d5c83fb085d7a163982
Julio Rodríguez <julioandres97@gmail.com>
2018-10-23 04:08:10 -0600

    Possible fix for R10 timeout error

--- LayerB/ServerExpress.js
+++ LayerB/ServerExpress.js
@@ -5,17 +5,22 @@ var app = express();
 const INDEX_URL = "/"
 const API_URL = "/api"
 
+// look for PORT environment variable, 
+// else look for CLI argument,
+// else use hard coded value for port 8080
+port = process.env.PORT || process.argv[2] || 8080;
+
 app.get(INDEX_URL, function (req, res) {
     let ip = req.header("x-forwarded-for");
     console.log("Accessed " + INDEX_URL + " from " + ip);
-    res.send('Hello World!!');
+    res.send('Administrador de estrategias de respaldo, Grupo 2');
 });
 
 app.get(API_URL, function (req, res) {
     res.send("This is the api.");
 });
 
-app.listen(3002, function () {
-  console.log('Example app listening on port 3002!');
+app.listen(port, function () {
+  console.log('App listening on port ' + port);
 });
 
