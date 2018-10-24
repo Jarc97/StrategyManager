cbd28f37da402c170314468eecc5fbd5b7f9094e
Julio Rodríguez <julioandres97@gmail.com>
2018-10-24 02:19:01 -0600

    New basic UI with CoreUI

--- LayerB/ServerExpress.js
+++ LayerB/ServerExpress.js
@@ -2,6 +2,7 @@
 var fs = require('fs');
 var express = require('express');
 var Dropbox = require('dropbox').Dropbox;
+var path = require('path');
 
 
 var app = express();
@@ -16,19 +17,34 @@ var dbx = function() {
     return new Dropbox({ accessToken: dropboxKey});
 }
 
-app.use(express.static(__dirname + "/../LayerA/"));
+// app.use(express.static(__dirname + "/../LayerA/"), function(req, res, next) {
+//     //console.log("Middleware for ALL requests");
+//     next();
+// });
+
+// app.use("/api", function(req, res, next) {
+//     console.log("Middleware for API requests");
+//     // WIP: verify client
+//     next();
+// });
+// app.use(express.static(__dirname + "/../LayerA/"));
+
+const LAYER_A = __dirname + "/../LayerA/src"
+const LAYER_B = __dirname
 
 const INDEX_URL = "/"
 const API_URL = "/api"
 // look for PORT environment variable, else look for CLI argument, else use hard coded value for port 8080
 const PORT = process.env.PORT || process.argv[2] || 8080;
 
+app.set("view engine", "pug");
 
 app.get(INDEX_URL, function (req, res) {
+    console.log("GET routing for INDEX")
     let ip = req.header("x-forwarded-for");
     console.log("Accessed " + INDEX_URL + " from " + ip);
     // res.send('Administrador de estrategias de respaldo, Grupo 2');
-    res.render("index.html");
+    res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 
@@ -39,5 +55,6 @@ app.get(API_URL, function (req, res) {
 
 app.listen(PORT, function () {
     console.log('App listening on port ' + PORT);
+    console.log(LAYER_A);
 });
 
